bc03cf35ffb54f0fee36d384f162ffc063820241
Alastair Smith <Alastair-smith2@users.noreply.github.com>
2021-04-03 15:29:27 +0100

    Assignment two
    Clean up tests

--- src/files/mod.rs
+++ src/files/mod.rs
@@ -1,4 +1,4 @@
-use url::{Host, Position, Url};
+use url::Url;
 
 pub type Result<T> = std::result::Result<T, std::io::Error>;
 
@@ -11,22 +11,11 @@ mod tests {
     use super::*;
     #[test]
     fn parse_url_works() {
-        let url = parse_url(String::from("https://docs.rs/lyon_core/0.8.0/lyon_core/")).unwrap();
-        assert!(url.scheme() == "https");
-        assert!(url.username() == "");
-        assert!(url.password() == None);
-        assert!(url.host_str() == Some("docs.rs"));
-        assert!(url.host() == Some(Host::Domain("docs.rs")));
-        assert!(url.port() == None);
-        assert!(url.path() == "/lyon_core/0.8.0/lyon_core/");
-        assert!(url.query() == None);
-        assert!(&url[Position::BeforePath..] == "/lyon_core/0.8.0/lyon_core/");
-        assert!(url.fragment() == None);
-        assert!(!url.cannot_be_a_base());
+        assert!(parse_url(String::from("https://docs.rs/lyon_core/0.8.0/lyon_core/")).is_some());
     }
 
     #[test]
     fn parse_url_fails() {
-        assert_eq!(parse_url(String::from("Fizz")), None)
+        assert!(parse_url(String::from("Fizz")).is_none());
     }
 }
